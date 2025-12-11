964f6f91b7d46a8f33d8b3e4890ddf9ace9f1c22
chames06 <oukelul@gmail.com>
2025-12-11 16:26:18 +0100

    e

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent | tee >(while read domain; do dig +short $domain A | head -1 | xargs -I {} echo "$domain,{}"; done > $1.txt)
+subfinder -d $1 -silent -oI -nW -o $1.txt
