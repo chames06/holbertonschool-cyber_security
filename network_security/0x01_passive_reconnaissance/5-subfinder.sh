f01acd281bda5dec4c9fcc90996f1113bc7aa2f9
chames06 <oukelul@gmail.com>
2025-12-11 16:17:20 +0100

    e

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent | tee /dev/tty | dnsx -silent -a -resp -o $1.txt
+subfinder -d $1 -silent | tee /dev/tty | dnsx -silent -a -resp -no-color | awk '{print $1","$3}' | tr -d '[]' > $1.txt
